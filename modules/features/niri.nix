7f481fa9388ebf6ae576d5634ca4dc304bf60fbf
alexis just <alexisjust@gmail.com>
2026-09-10 14:52:27 +0200

    noctalia wip

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -9,9 +9,17 @@
 
 
 
-  perSystem = { pkgs, lib, ... }: {
+  perSystem = { pkgs, lib, self', ... }: {
     packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
       inherit pkgs;
+      settings = {
+	spawn-at-startup =[
+	  (lib.getExe self'.packages.myNoctalia)
+	];
+	#binds = {
+	#  "Mod+D".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
+	#};
+      };
       extraSettings = [ { include = ./niri-config.kdl; } ];
     };
 
